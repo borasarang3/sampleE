1f9aceb14c03cb7c305da5dcee81fd5f4dab7c86
eunununu <eunununu@naver.com>
2024-12-06 13:05:33 +0900

    Eu Controller, DTO, Service, Repoistory, register.html

--- src/main/java/com/example/demo/controller/EuController.java
+++ src/main/java/com/example/demo/controller/EuController.java
@@ -1,18 +1,41 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.EuDTO;
+import com.example.demo.service.EuService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Log4j2
 @RequiredArgsConstructor
+@RequestMapping("/eu")
 public class EuController {
 
-    @GetMapping("/eu/list")
+    private final EuService euService;
+
+    @GetMapping("/list")
     public String euList() {
 
         return "eu/list";
     }
-}
+
+    @GetMapping("/register")
+    public String euRegister(){
+
+        return "eu/register";
+    }
+
+    @PostMapping("/register")
+    public String euRegisterPost(EuDTO euDTO){
+        euService.register(euDTO);
+
+        return "eu/list";
+    }
+
+
+    }

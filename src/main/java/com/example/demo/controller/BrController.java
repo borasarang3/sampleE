15ac90f7aac7d2012aee3bd701963db657fa58f8
bora <dianabora@naver.com>
2024-12-06 12:58:55 +0900

    Br 게시판 글 등록

--- src/main/java/com/example/demo/controller/BrController.java
+++ src/main/java/com/example/demo/controller/BrController.java
@@ -1,17 +1,36 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.BrDTO;
+import com.example.demo.service.BrService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Log4j2
 @RequiredArgsConstructor
+@RequestMapping("/br")
 public class BrController {
 
-    @GetMapping("/br/list")
+    private final BrService brService;
+
+    @GetMapping("/list")
     public String brList () {
+        return "br/list";
+    }
+
+    @GetMapping("/register")
+    public String brRegister() {
+        return "br/register";
+    }
+
+    @PostMapping("/register")
+    public String brRegisterPost(BrDTO brDTO) {
+        brService.register(brDTO);
+
         return "br/list";
     }
 

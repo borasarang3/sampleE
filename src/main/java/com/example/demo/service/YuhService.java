f1e8757de3899dcbd9c5027b41b27ae86e4ef3f0
lotus <lotus@a.a>
2024-12-06 14:16:13 +0900

    추가 repository/ service

---
+++ src/main/java/com/example/demo/service/YuhService.java
@@ -0,0 +1,17 @@
+package com.example.demo.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Log4j2
+@Transactional
+public class YuhService {
+
+    //그냥 서비스 만듬
+
+
+}

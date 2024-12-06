777fbb973e494d70038fa34b311098118368b43b
lotus <lotus@a.a>
2024-12-06 12:09:27 +0900

    yuh 컨트롤러

---
+++ src/main/java/com/example/demo/controller/yuhController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+@Log4j2
+public class yuhController {
+
+    @GetMapping("/yuh/list")
+    public String lotus(){
+
+        return "yuh/list";
+    }
+
+}
